6e23d32ba4f8de4bc325c517cede4345b5835ac5
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-10-01 17:26:47 +0200

    Creación de los compomentes de paginacion y el el grid de productos

--- src/shop/page/home/HomePage.tsx
+++ src/shop/page/home/HomePage.tsx
@@ -1,10 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products.mocks";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los productos" />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
     </>
   );
 };
