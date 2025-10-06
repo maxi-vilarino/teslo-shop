0dda82a67cc420ded1e01b7625a48ea9f6ecd0da
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-10-06 10:00:27 +0200

    Creación de interfaces y llamada a endpoint de productos

--- src/shop/page/home/HomePage.tsx
+++ src/shop/page/home/HomePage.tsx
@@ -2,8 +2,10 @@ import { CustomPagination } from "@/components/custom/CustomPagination";
 import { products } from "@/mocks/products.mocks";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
 import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useProducts } from "@/shop/hooks/useProducts";
 
 export const HomePage = () => {
+  const { data } = useProducts();
   return (
     <>
       <CustomJumbotron title="Todos los productos" />
