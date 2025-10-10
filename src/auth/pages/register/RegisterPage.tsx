663dbcb6344b80f78e8613cc8a9fa53431c8d413
Maximiliano Vilariño <mvilarino@aldaba.es>
2025-10-10 09:32:50 +0200

    Función registrar usuario

--- src/auth/pages/register/RegisterPage.tsx
+++ src/auth/pages/register/RegisterPage.tsx
@@ -1,16 +1,37 @@
+import { useState, type FormEvent } from "react";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { CustomLogo } from "@/components/custom/CustomLogo";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useAuthStore } from "@/auth/store/auth.store";
 
 export const RegisterPage = () => {
+  const navigate = useNavigate();
+  const { register } = useAuthStore();
+  const [isPosting, setIsPosting] = useState(false);
+  const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsPosting(true);
+    const formData = new FormData(event.target as HTMLFormElement);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const fullName = formData.get("password") as string;
+    const isRegister = await register(email, password, fullName);
+    if (isRegister) {
+      navigate("/");
+      return;
+    }
+    toast.error("Correo y/o contraseña no validos");
+    setIsPosting(false);
+  };
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleRegister}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <CustomLogo />
@@ -22,6 +43,7 @@ export const RegisterPage = () => {
                 <Label htmlFor="fullName">Nombre completo</Label>
                 <Input
                   id="fullName"
+                  name="fullName"
                   type="text"
                   placeholder="John Doe"
                   required
@@ -31,6 +53,7 @@ export const RegisterPage = () => {
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
+                  name="email"
                   type="email"
                   placeholder="mail@example.com"
                   required
@@ -49,11 +72,12 @@ export const RegisterPage = () => {
                 <Input
                   id="password"
                   type="password"
+                  name="password"
                   placeholder="Contraseña"
                   required
                 />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPosting}>
                 Registrarse
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
--- src/auth/store/auth.store.ts
+++ src/auth/store/auth.store.ts
@@ -2,6 +2,7 @@ import type { User } from "@/interfaces/user.interface";
 import { create } from "zustand";
 import { loginAction } from "../actions/login.action";
 import { checkAuthAction } from "../actions/check-auth.action";
+import { registerAction } from "../actions/register.action";
 
 type AuthStatus = "authenticated" | "not-authenticated " | "checking";
 
@@ -10,6 +11,11 @@ type AuthState = {
   token: string | null;
   authStatus: AuthStatus;
   isAdmin: () => boolean;
+  register: (
+    email: string,
+    password: string,
+    fullName: string
+  ) => Promise<boolean>;
   login: (email: string, password: string) => Promise<boolean>;
   logout: () => void;
   checkAuthStatus: () => Promise<boolean>;
@@ -23,14 +29,27 @@ export const useAuthStore = create<AuthState>()((set, get) => ({
     const roles = get().user?.roles || [];
     return roles.includes("admin");
   },
+  register: async (email: string, password: string, fullName: string) => {
+    try {
+      const data = await registerAction(email, password, fullName);
+      localStorage.setItem("token", data.token);
+      set({ user: data.user, token: data.token, authStatus: "authenticated" });
+      return true;
+    } catch (error) {
+      console.error(error);
+      localStorage.removeItem("token");
+      set({ user: null, token: null, authStatus: "not-authenticated " });
+      return false;
+    }
+  },
   login: async (email: string, password: string) => {
-    console.log({ email, password });
     try {
       const data = await loginAction(email, password);
       localStorage.setItem("token", data.token);
       set({ user: data.user, token: data.token, authStatus: "authenticated" });
       return true;
     } catch (error) {
+      console.error(error);
       localStorage.removeItem("token");
       set({ user: null, token: null, authStatus: "not-authenticated " });
       return false;
